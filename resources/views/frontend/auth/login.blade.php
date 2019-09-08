8b867da3c97baabcb1b5f860e89cd17f12770da2
hallitee <hallitee_2005@yahoo.com>
2019-09-09 00:55:05 +0100

    still working on login

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -39,22 +39,34 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+                  {{ html()->form('POST', route('frontend.auth.login.post'))->class('user')->open() }}
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+					                   {{ html()->email('email')
+                                        ->class('form-control form-control-user')
+                                        ->placeholder('Enter '.__('validation.attributes.frontend.email'))										
+                                        ->attribute('maxlength', 191)
+                                        ->required() }}
+               
                     </div>
+					<br>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      {{ html()->password('password')
+                                        ->class('form-control')
+                                        ->placeholder(__('validation.attributes.frontend.password'))
+                                        ->required() }}
                     </div>
+					<br>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <input type="checkbox" name="remember" class="custom-control-input" id="customCheck">
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
                     <a href="index.html" class="btn btn-primary btn-user btn-block">
                       Login
                     </a>
+					<!--
                     <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
@@ -62,11 +74,17 @@
                     <a href="index.html" class="btn btn-facebook btn-user btn-block">
                       <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                     </a>
-                  </form>
+					-->
+                  {{ html()->form()->close() }}
+				  <br>
+				  <br>
                   <hr>
+				  <br>
+				  <br>				  
                   <div class="text-center">
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
+				  <br>
                   <div class="text-center">
                     <a class="small" href="register.html">Create an Account!</a>
                   </div>
